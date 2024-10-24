ddf6df2862ab457d20f333229d2a34b88ace9cec
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2024-10-24 12:59:36 -0300

    Included in Program.cs the functionality of serving static files to the API

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -107,8 +108,14 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+    // https://localhost:1234/Images
+});
 
 app.MapControllers();
 
